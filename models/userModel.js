5db874ef6bbb6d26dd357a108ef6cb1907bead52
Shailesh Patro <shailesh.patro2002@gmail.com>
2022-03-08 00:41:00 +0530

    commit 2

---
+++ models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const userSchema = new mongoose.Schema({
+  uniqueId: {
+    type: String,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  googleId: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: [true, "Email should be unique"],
+    trim: true,
+  },
+  collegeName: {
+    type: String,
+    required: [true, "College is required"],
+    trim: true,
+  },
+  isNitr: {
+    type: Boolean,
+    required: [true, "IsNitr is required"],
+  },
+  nitrMail: {
+    type: String,
+    trim: true,
+  },
+  participatedEvents: {
+    type: Array,
+  },
+  registered: {
+    type: Boolean,
+    default: false,
+  },
+});
+userSchema.plugin(findOrCreate);
+module.exports = mongoose.model("User", userSchema);
