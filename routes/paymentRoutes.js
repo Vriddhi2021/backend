9174f059343567c7d44baafc426b623b3ce08e1d
Aditya <Adi72Hzb@gmail.com>
2022-03-22 02:59:50 +0530

    payment

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,18 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 
-router.post("/", async (req, res) => { 
+router.post("/test", async (req, res) => { 
     try{
         const user = await User.findOne({
-            uniqueId: req.body.userid
+            uniqueId: req.body.userid.toLowerCase()
           });
-          await user.updateOne({paidStatus : true});
-        res.status(200).json({
-            message : "Successfull"
-            });
+
+          if(user){
+            await user.updateOne({paidStatus : true});
+            res.status(200).json({
+                message : "Successfull"
+                });
+          }
+          else{
+            res.status(200).json({
+                message : "USER NOT FOUND"
+                });
+          }  
     }catch(err){
         console.log(err);
-        res.status(400).json({
+        res.status(200).json({
         status: "Failed to update Payment Details.PLZ contact with organizers",
         message: err
         })
